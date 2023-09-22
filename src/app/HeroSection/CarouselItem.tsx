b0132d9bda8d91e26e9730dd94b8903e598bd663
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-09-22 18:41:11 +0530

    small changes

--- src/app/HeroSection/CarouselItem.tsx
+++ src/app/HeroSection/CarouselItem.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const CarouselItem = ({ title1, title2, description, imageUrl }: { title1: string; title2: string; description: string; imageUrl: string }) => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:px-20 2xl:px-32">
-      <div className="flex flex-col md:flex-row justify-evenly h-screen items-center ">
+    <div className="px-4 md:px-8 lg:px-16 xl:px-20 2xl:px-32 mt-20">
+      <div className="flex flex-col-reverse md:flex-row justify-evenly h-screen items-center ">
         <div className="w-full max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl">
           <div className="flex flex-col justify-center p-4">
             <h2 className="text-4xl font-extrabold text-black">{title1}<br />{title2}</h2>
