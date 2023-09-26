aad6bc103a204c606ad9368baecdab36fefc4d39
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-09-26 12:54:32 +0530

    changes

--- src/app/Components/ContentBox.tsx
+++ src/app/Components/ContentBox.tsx
@@ -4,13 +4,13 @@ import { BsArrowUpRight } from 'react-icons/bs';
 const ContentBox = ({ header, para, link }: any) => {
   return (
     <div className="h-fit font-serif flex flex-col p-4 gap-y-5 sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 ">
-      <h2 className="text-2xl font-bold text-blue-500">{header}</h2>
-      <div>
+      <h2 className="text-3xl font-bold text-blue-500">{header}</h2>
+      <div className='text-xl'>
         <p>{para}</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 gap-y-4">
         {link?.map((item: string, index: number) => (
-          <a href="#" key={index} className="text-green-400 flex items-center font-semibold hover:text-green-600">
+          <a href="#" key={index} className="text-green-400 text-lg flex items-center font-semibold hover:text-green-600">
             {item}
             <BsArrowUpRight size={15} className="ml-2" />
           </a>
