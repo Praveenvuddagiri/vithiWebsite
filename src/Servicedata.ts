40ad46e6e74804a3d408b89a99673eab3be05025
Sudheer Chakka <chsudheer08@gmail.com>
2023-09-27 17:32:22 +0530

    services data added

---
+++ src/Servicedata.ts
@@ -0,0 +1,54 @@
+[
+    {
+      slug: "application-development",
+      top: {
+        image:"https://www.vithiitsolutions.com/images/4e0fecc1a564f2bedda9d6e00db11dda.png",
+        title:"Custom technology solutions specific to your industry and business.",
+        navigations:["services", "Aplication Development"]
+      },
+      about: {
+        title: "Application Development",
+        heading:"Vithi IT Solutions builds custom applications using existing and emerging technologies that work well with any software portfolio to meet specific business requirements. We can create solutions for meeting the most challenging IT problems.",
+        para:"We combine deep technology expertise, architecture solutions capability, and program management skills to help you integrate and acquire new capabilities for heterogeneous systems that span both mainstream and emerging technologies. Specialists across our delivery network work with you to provide the skills you need, when you need them, for streamlining, managing and maximizing all systems, applications and enterprise platforms."
+      },
+      expertise: 
+      [
+        {
+          technology: "ERP Solutions",
+          data: {
+            image:"https://www.vithiitsolutions.com/images/3b0375ebc706f5bbef6341940447658b.jpg",
+            title:"ERP Solutions ",
+            description:["We offer custom ERP software development services for businesses of all sizes and industries. We develop robust and scalable business ERP systems that offer key data insights across multiple platforms - web, mobile, and on-premise." ,"The best of ERP solutions count for nothing if they are not implemented properly. Our ERP implementation service team is uniquely equipped to take care of the complete deployment of your custom networks, servers, and data management solutions while maintaining data integrity.","Our technical experts using the best industry technologies and programming languages build feature-rich custom ERP applications as per the needs of your business. Our experts develop the best-in-class ERP applications for all platforms including web, desktop, and mobile."]
+          }
+        },
+        {
+            technology:"Full Stack Web Development ",
+            data:{
+                image:"https://www.vithiitsolutions.com/images/0c788c2c60c9f67dc13d2d7c11663892.jpg",
+                title:"Full Stack Web Development" ,
+                description:["Whether you need to build a custom web app from scratch, migrate your legacy backend, or streamline existing front-end functionality, we can accomplish that in an efficient and cost-effective manner.","We offer business intelligence solutions, application migration to cloud, collaboration and portal development, document management solutions, secure intranets, extranets, and portal setup, and offer maintenance, support, and up-gradation."]
+            }
+        },
+        {
+            technology:"CRM",
+            data:{
+                image:"https://www.vithiitsolutions.com/images/d3e8d3a844e4b2e451eaaf160a913ae5.jpg",
+                title:"CRM",
+                description:["Customers are the most important part of any organization, and we offer custom crm development services that secure your customers’ loyalty to your company.We create the kind of CRM technology tools that provide direct access to customer information, which is essential for evaluating marketing strategies and customer service processes.","Vithi IT Solutions delivers Salesforce customer relationship management (CRM) and customer experience management (CEM) solutions through sales, service, and marketing cloud in addition to custom platform-as-a-service (PaaS) applications that provide the foundation for sustainable growth and increased success. Our in-depth processes and application knowledge, combined with best practices, enable us to offer Salesforce services, including consultancy, implementation, and support.","We also provide HubSpot CRM solutions, Social Media marketing services, Content Management Services with web Analytic feature."]
+            }
+        },
+        {
+            technology:"Mobile Development ",
+            data:{
+                image:"https://www.vithiitsolutions.com/images/fabfbddf27562ee338f33adfdf063448.jpg",
+                title:"Mobile Development " ,
+                description:["We offer a full cycle of application design, integration and management services. Whether it is a consumer oriented app or a transformative enterprise-class solution, the company leads the entire mobile app development process from ideation and concept to delivery, and to ongoing ongoing support.","We have all the expertise you need to produce fully-fledged, stable, and scalable mobile applications."],
+                workList:{
+                title: "Types of Mobile App Development",
+                list:["Native Mobile App Development","Hybrid Mobile App Development "],
+                }
+            }
+        }
+        ]
+    }
+]
