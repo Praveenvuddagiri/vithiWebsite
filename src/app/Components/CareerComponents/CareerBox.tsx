0cdb0949b383c4ac8ed59c89192b0cdfac493008
Sudheer Chakka <chsudheer08@gmail.com>
2023-09-28 18:43:57 +0530

    career changes

--- src/app/Components/CareerComponents/CareerBox.tsx
+++ src/app/Components/CareerComponents/CareerBox.tsx
@@ -4,23 +4,28 @@ import CareersBoxBenefit from './CareersBoxBenefit';
 
 const CareerBox = () => {
   return (
-    <div className='bg-gradient-to-r from-[#3F51B5] to-[#269cca] '>
+    <div className='bg-gradient-to-r from-[#3F51B5] to-[#269cca] p-4 md:p-14 relative'>
+      <img
+        src="https://www.vithiitsolutions.com/images/9fda3ccbc09fd6b1db76410e59c325d0.png"
+        alt="background image"
+        className='w-full h-full absolute inset-0 object-cover opacity-10'
+      />
       <div className=''>
-        <h2>A Career With Real Benefits</h2>
+        <h2 className='text-[24px] md:text-[36px] leading-[36px] text-center mb-[15px] md:mb-[30px] text-white capitalize font-[700]'>
+          A Career With Real Benefits
+        </h2>
       </div>
-      <div>
-        {
-          careersdata?.map((e)=>{
-            return (
-              <div>
-                <CareersBoxBenefit image={e.image} title={e.title} content={e.content}/>
-              </div>
-            )
-          })
-        }
+      <div className='grid gap-7 sm:grid-cols-12'>
+        {careersdata?.map((e, index) => {
+          return (
+            <div className='col-span-4 md:col-span-4' key={index}>
+              <CareersBoxBenefit image={e.image} title={e.title} content={e.content} />
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CareerBox
+export default CareerBox;
--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import CareerBox from '../Components/CareerComponents/CareerBox'
+import React from "react";
+import CareerBox from "../Components/CareerComponents/CareerBox";
+import Navbar from "../Components/Navbar";
+import TopBreadCrum from "../Components/TopBreadCrum";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Careers",
+  description: "VITHI IT SOLUTIONS",
+};
+
+const aboutUsTopData = {
+    title: 'Were always looking for talented people.',
+    image: 'https://www.vithiitsolutions.com/images/9fda3ccbc09fd6b1db76410e59c325d0.png',
+    navigations: ['Careers']
+  }
 
 const page = () => {
   return (
     <div>
+      <Navbar active="aboutus" />
+      <div className='mt-24'>
+        <TopBreadCrum title={aboutUsTopData.title} image={aboutUsTopData.image} navigations={aboutUsTopData.navigations}/>
+      </div>
       <CareerBox />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
