85287b420f1ec8f5d16737d3e1631057984c00e7
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-09-29 12:40:22 +0530

    added contact data and jobs section

--- src/app/Components/CareerComponents/JobsSection.tsx
+++ src/app/Components/CareerComponents/JobsSection.tsx
@@ -9,22 +9,25 @@ const JobsSection = () => {
 
     const jobPosts = jobs;
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center mt-7">
+            <h1 className="text-[#1e85bd] md:text-[36px] font-bold text-[20px]">OPEN POSITIONS</h1>
+            <div className="w-[85%] mt-4">
 
-            {
-                jobPosts?.map((job: any, index: number) =>
-                    <div key={index}>
-                        <PostAccordion
-                            postData={job}
-                            active={(index === currentAccordion) ? true : false}
-                            postNumber={index + 1}
-                            setCurrentAccordion={setCurrentAccordion}
-                            isOpen={isOpen}
-                            setIsOpen={setIsOpen}
-                        />
-                    </div>
-                )
-            }
+                {
+                    jobPosts?.map((job: any, index: number) =>
+                        <div key={index}>
+                            <PostAccordion
+                                postData={job}
+                                active={(index === currentAccordion) ? true : false}
+                                postNumber={index + 1}
+                                setCurrentAccordion={setCurrentAccordion}
+                                isOpen={isOpen}
+                                setIsOpen={setIsOpen}
+                            />
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -3,6 +3,7 @@ import CareerBox from "../Components/CareerComponents/CareerBox";
 import Navbar from "../Components/Navbar";
 import TopBreadCrum from "../Components/TopBreadCrum";
 import { Metadata } from "next";
+import JobsSection from "../Components/CareerComponents/JobsSection";
 
 export const metadata: Metadata = {
   title: "Careers",
@@ -10,19 +11,21 @@ export const metadata: Metadata = {
 };
 
 const aboutUsTopData = {
-    title: 'Were always looking for talented people.',
-    image: 'https://www.vithiitsolutions.com/images/9fda3ccbc09fd6b1db76410e59c325d0.png',
-    navigations: ['Careers']
-  }
+  title: 'Were always looking for talented people.',
+  image: 'https://www.vithiitsolutions.com/images/9fda3ccbc09fd6b1db76410e59c325d0.png',
+  navigations: ['Careers']
+}
 
 const page = () => {
   return (
     <div>
       <Navbar active="aboutus" />
       <div className='mt-24'>
-        <TopBreadCrum title={aboutUsTopData.title} image={aboutUsTopData.image} navigations={aboutUsTopData.navigations}/>
+        <TopBreadCrum title={aboutUsTopData.title} image={aboutUsTopData.image} navigations={aboutUsTopData.navigations} />
       </div>
       <CareerBox />
+
+      <JobsSection />
     </div>
   );
 };
