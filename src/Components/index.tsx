6148fb4a8e91faced60d060ad44ac6b780f33483
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-09-30 17:33:38 +0530

    completed index.tsx imports

--- src/Components/index.tsx
+++ src/Components/index.tsx
@@ -4,8 +4,6 @@ export {default as Navbar} from '@/Components/CommonComponents/Navbar';
 export {default as MoveToUp} from '@/Components/CommonComponents/MoveToUp';
 export {default as TopBreadCrum} from '@/Components/CommonComponents/TopBreadCrum';
 
-
-
 //Home Components here
 export {default as VideoBackgroundComponent} from '@/Components/HomeComponents/VideoBackgroundComponent';
 export {default as FullContactForm} from '@/Components/HomeComponents/FullContactForm';
@@ -13,4 +11,30 @@ export {default as HomeServicesBox} from '@/Components/HomeComponents/HomeServic
 export {default as ImageComponent} from '@/Components/HomeComponents/ImageComponent';
 export {default as ContentBox} from '@/Components/HomeComponents/ContentBox';
 export {default as Hero} from '@/Components/HomeComponents/HeroSection';
+
+//About Components here
+export {default as AboutContentBox} from '@/Components/AboutComponents/AboutContentBox';
+export {default as AboutVisionBox} from '@/Components/AboutComponents/AboutVisionBox';
+export {default as CoreValuesList} from '@/Components/AboutComponents/CoreValues';
+export {default as About} from '@/Components/AboutComponents/KeyDifferences';
+
+//Service Components here
+export {default as ExpertiseBox } from '@/Components/ServiceComponents/ExpertiseBox';
+export {default as ServiceAboutContentBox } from '@/Components/ServiceComponents/ServiceAboutContentBox';
+
+//Career Components here
+export {default as CareerAboutBox } from '@/Components/CareerComponents/CareerAboutBox';
+export {default as CareerBox } from '@/Components/CareerComponents/CareerBox';
+export {default as CareerContactForm } from '@/Components/CareerComponents/CareerContactForm';
+export {default as CareersBoxBenefit } from '@/Components/CareerComponents/CareersBoxBenefit';
+export {default as JobsSection } from '@/Components/CareerComponents/JobsSection';
+
+
+//Contact Components here
+export {default as ContactDetails } from '@/Components/ContactComponent/ContactDetails';
+export {default as ContactForm } from '@/Components/ContactComponent/ContactForm';
+
+
+
+
 
