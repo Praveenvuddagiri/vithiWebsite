49331a7fe48d87941b212235a847c40d2c5b904a
Ramesh <ramesh.gorekanti@gmail.com>
2023-10-03 17:37:44 +0530

    footerChanges

--- src/Components/CommonComponents/Footer/index.tsx
+++ src/Components/CommonComponents/Footer/index.tsx
@@ -4,9 +4,7 @@ import { TiSocialLinkedinCircular } from 'react-icons/ti';
 import { footerData } from '@/utilities/constant';
 import FooterDataSection from './FooterDataSection';
 
-
-
-const Footer = () => {
+const Footer = ({ logoSrc, logoAlt, h6Content, h2Content, facebookLink, linkedinLink }:any) => {
     return (
         <div className="flex flex-col items-center justify-center mt-10">
             <div className="max-w-full">
@@ -14,31 +12,31 @@ const Footer = () => {
                     <div className='m-2 w-96 md:col-span-2 leading-[24px] font-[400] text-[13px]'>
                         <img
                             className="w-60 h-auto mb-2 md:mb-4"
-                            src="https://www.vithiitsolutions.com/images/logo.png"
-                            alt="Vithi IT Solutions Logo"
+                            src={logoSrc}
+                            alt={logoAlt}
                         />
 
                         <h6>
-                            Vithi IT Solutions is a <strong>Strategic Technology Partner</strong> helping companies achieve operational excellence by providing the best solutions in the form of End-to-End software solutions after analyzing the user’s needs and problems <br />
+                            {h6Content}
                         </h6>
                         <a href="#" className='border-b-2 border-b-gray-600 text-gray-700 font-[600] uppercase'>Read More</a>
                         <div className="mt-4 md:mt-2 flex items-center gap-4">
-                            <h2 className="text-[#1e85bd] text-sm font-[700] mt-1">Get Social With Us</h2>
+                            <h2 className="text-[#1e85bd] text-sm font-[700] mt-1">{h2Content}</h2>
                             <ul className="flex items-center mt-2 text-center mb-4">
                                 <li>
-                                    <a href="https://www.facebook.com">
+                                    <a href={facebookLink}>
                                         <BsFacebook size={30} className='hover:text-[#0f1941] text-[#31a2df]' />
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="https://www.linkedin.com/company/vithi-it-solutions">
+                                    <a href={linkedinLink}>
                                         <TiSocialLinkedinCircular size={43} className='hover:text-[#0f1941] text-[#31a2df]' />
                                     </a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    {footerData?.map((section: any, index: number) => (
+                    {footerData?.map((section, index) => (
                         <FooterDataSection key={index} sectionData={section} />
                     ))}
                 </div>
@@ -51,4 +49,3 @@ const Footer = () => {
     );
 };
 
-export default Footer;
