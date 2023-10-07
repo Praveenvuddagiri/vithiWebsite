b702b1426275a6fce4ee511aa4a66aeb7004a1ad
Ramesh <ramesh.gorekanti@gmail.com>
2023-10-07 15:09:42 +0530

    changes

--- src/Components/AboutComponents/AboutContentBox.tsx
+++ src/Components/AboutComponents/AboutContentBox.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const AboutContentBox = ({ title, heading, content }: { title: string, heading: string, content: string }) => {
     return (
-        <div className='w-full'>
+        <div className='w-full bg-primary'>
             <div className='flex flex-col items-start justify-center gap-8 md:px-6 p-4 md:p-8 lg:p-16 xl:p-28'>
-                <h1 className='text-primary font-bold text-sm md:text-3xl'>
+                <h1 className='text-secondary font-bold text-sm md:text-3xl'>
                     {title}
                 </h1>
-                <p className='text-2xl font-bold'>
+                <p className='text-2xl font-bold text-white'>
                     {heading}
                 </p>
-                <p className='text-lg md:text-xl lg:text-2xl text-gray-800'>
+                <p className='text-lg md:text-xl lg:text-2xl text-white'>
                     {content}
                 </p>
             </div>
--- src/Components/CareerComponents/CareerAboutBox.tsx
+++ src/Components/CareerComponents/CareerAboutBox.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 
 const CareerAboutBox = ({title, content, contact}:{title:string, content:string, contact: string}) => {
   return (
-    <div className='flex justify-center md:p-20'>
+    <div className='flex justify-center md:p-20 bg-primary'>
     <div className='p-8 w-[85%]'>
-      <div className='md:text-[48px] leading-[36px] text-[#000000] text-center font-bold mb-[30px] capitalize mt-0 sm:text-[30px] sm:leading-6'>
+      <div className='md:text-[48px] leading-[36px] text-secondary text-center font-bold mb-[30px] capitalize mt-0 sm:text-[30px] sm:leading-6'>
         <h1>{title}</h1>
       </div>
-      <div className='text-[#697c86] text-[16px] leading-[32px] text-center font-[400] p-4'>
+      <div className='text-white text-[16px] leading-[32px] text-center font-[400] p-4'>
         
           <p >{content}</p>
           <p >{contact}</p>
