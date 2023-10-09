f90f15ebc0d79ef077194dd90ea310ada381d5fc
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-10-09 15:48:52 +0530

    changes in config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './.next/static/**/*.{html,js}',
+    './.next/**/*.{html,js}',
     './public/**/*.{html,js}',
     './static/**/*.{html,js}',
   ],
