42246a804c45d5f540dcdf6b8bd9105276bf213b
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2023-10-10 12:28:12 +0530

    responsive changes

--- src/Components/HomeComponents/HeroSection/CarouselItem.tsx
+++ src/Components/HomeComponents/HeroSection/CarouselItem.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 const CarouselItem = ({ title1, title2, description, imageUrl, discussButton }: { title1: string; title2: string; description: string; imageUrl: string, discussButton: any }) => {
   return (
     <div className="mb-16 animate-fade animate-once animate-duration-500 animate-delay-0 animate-ease-in-out">
-      <div className="flex relative flex-col-reverse md:flex-row justify-between h-screen items-center ">
-        <div className='absolute z-0 md:-bottom-14 -bottom-28'>
+      <div className="flex relative flex-col-reverse md:flex-row justify-between h-[90vh] items-center ">
+        <div className='absolute z-0 xl:-bottom-14 md:bottom-24 -bottom-28'>
           <video src="http://www.vithiitsolutions.com/images/particles.mp4" autoPlay loop muted
             className='opacity-25 w-[52rem]'></video>
         </div>
-        <div className="px-4 md:px-4 lg:px-8 xl:px-16 2xl:px-24 z-10 max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl w-[90%] md:w-full animate-fade-up animate-once animate-delay-100 animate-ease-in-out">
+        <div className="min-h- px-4 md:px-5 lg:px-8 xl:px-16 2xl:px-24 z-10 max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl w-[90%] md:w-full animate-fade-up animate-once animate-delay-100 animate-ease-in-out">
           <div className="flex flex-col justify-center p-4 items-center md:items-start">
-            <h2 className="text-3xl md:text-5xl font-bold leading-10 text-primary ">{title1}<br />{title2}</h2>
+            <h2 className="text-3xl md:text-3xl xl:text-5xl font-bold leading-10 text-primary ">{title1} {title2}</h2>
             <p className="mt-5 text-lg text-light leading-7 text-justify">{description}</p>
 
             <Link href={discussButton.link}>
@@ -21,8 +21,8 @@ const CarouselItem = ({ title1, title2, description, imageUrl, discussButton }: 
             </Link>
           </div>
         </div>
-        <div className="z-10 md:pr-16 w-full max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl">
-          <img src={imageUrl} alt={title1} className="w-full h-auto object-cover animate-fade animate-once animate-duration-[1000ms] animate-delay-[100ms] animate-ease-in" />
+        <div className="z-10 w-full max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl">
+          <img src={imageUrl} alt={title1} className="w-[90%] h-auto object-cover animate-fade animate-once animate-duration-[1000ms] animate-delay-[100ms] animate-ease-in" />
         </div>
       </div>
     </div>
