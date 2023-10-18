4d1caad501abaddefef6f3a98a79af5dcd50906d
Ramesh <ramesh.gorekanti@gmail.com>
2023-10-18 15:12:03 +0530

    nextChanges

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // images: {
+  //   loader: 'custom',
+  //   loaderFile: './my-loader.ts',
+  // },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
+        
       }
 }
 
 module.exports = nextConfig
+
